2cbeb3f09f5974735e027088772d5822d8c6b9d9
danieldolz <danieldolz@iesebre.com>
2016-10-21 17:33:50 +0200

    bd & tests1

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -29,6 +29,6 @@ $factory->define(App\Task::class, function (Faker\Generator $faker) {
     return [
         'name'      =>  $faker->sentence,
         'done'      =>  $faker->boolean(),
-        'priority'  =>  $faker->sentence,
+        'priority'  =>  $faker->integer,
     ];
 });
--- tests/TasksApiTest.php
+++ tests/TasksApiTest.php
@@ -4,13 +4,40 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+/**
+ * Class TasksApiTest
+ */
 class TasksApiTest extends TestCase
 {
+    protected $uri = '/api/task';
+
+    use DatabaseMigrations;
 
     public function testShowAllTasks()
     {
-        $this->json('GET', '/api/task')
+        $this->json('GET', $this->uri)
 //             ->dump();
-                ->seeJson();
+              ->seeJson();
     }
+
+
+    // Group failing
+    public function testShowOneTask()
+    {
+        $task = factory(App\Task::class)->create();
+        $id = 1;
+        $this->json('GET', $this->uri . '/' . $task->id)
+//            ->dump();
+//            ->seeJson();
+            ->seeJsonStructure(
+                ["id", "name", "done", "priority"]
+            )
+            ->seeJsonContains([
+                "name" => $task->name,
+                "done" => $task->done,
+                "priority" => $task->priority
+            ]);
+    }
+
+
 }
